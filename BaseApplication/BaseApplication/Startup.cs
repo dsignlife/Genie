2e560d650d0e8632a04379b6ec9a9d40cb7f93ad
Tomz <dsign-life@hotmail.com>
2018-12-31 12:28:36 +0100

    add controllers, create database project

--- BaseApplication/BaseApplication/Startup.cs
+++ BaseApplication/BaseApplication/Startup.cs
@@ -15,20 +15,27 @@ namespace BaseApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseNodeModules(env);
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            app.UseMvc(config =>
+            {
+                config.MapRoute("Default", "/{controller}/{action}/{id?}", new { controller = "App", Action = "Index" });
+            });
 
+
+            //app.UseNodeModules(env);
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Hello World!");
