5820b0a3ad7ba8934b5b217eb1252aeaedb297a2
Tomz <dsign-life@hotmail.com>
2018-12-31 11:36:44 +0100

    update npm, gitignore. add dependencyInjection

--- BaseApplication/BaseApplication/Startup.cs
+++ BaseApplication/BaseApplication/Startup.cs
@@ -20,15 +20,19 @@ namespace BaseApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseNodeModules(env);
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
